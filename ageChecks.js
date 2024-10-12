c8f2359777115c7de21f614e0d9bd15ff6022069
kulotskiI <ishielmishma24@gmail.com>
2024-10-12 14:56:06 +0800

    Inital commint

---
+++ ageChecks.js
@@ -0,0 +1,16 @@
+// IS STATEMENT
+// let age = 14;
+// if (age >=19)
+// else if ( age <=18)
+//console.log("You are a teenager");
+//  console.log("you are an adult");
+// }
+
+//ELSE STATEMENT
+let age = 14;
+if (age <=13)
+    console.log("you are an adult");
+else if ( age >=18)
+    console.log("You are a teenager");
+else
+    console.log("you are not an adult");
